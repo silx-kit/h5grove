d4bda3543fcab8029be81f19762ef07073fa9cd4
Thomas VINCENT <thomas.vincent@esrf.fr>
2021-05-31 15:04:09 +0200

    Add bson encoding

--- h5core/encoders.py
+++ h5core/encoders.py
@@ -1,6 +1,7 @@
 import io
 from numbers import Number
 from typing import Dict, Generator, NamedTuple, Optional, Sequence, Union
+import bson
 import numpy as np
 import orjson
 import h5py
@@ -21,6 +22,12 @@ def default(o) -> Union[list, str, None]:
 
 def orjson_encode(content):
     return orjson.dumps(content, default=default, option=orjson.OPT_SERIALIZE_NUMPY)
+
+
+def bson_encode(content) -> bytes:
+    return bson.dumps(
+        content if isinstance(content, dict) else {"": content}, on_unknown=default
+    )
 
 
 def npy_stream(array: Sequence[Number]) -> Generator[bytes, None, None]:
@@ -67,6 +74,7 @@ def encode(content, encoding: Optional[str] = "json") -> Response:
     :param content:
     :param encoding:
         - `json` (default)
+        - `bson`
         - `npy`: Only nD array-like of numbers is supported
     :returns: A Response object providing:
         - encoded `content` either as bytes or a generator of bytes
@@ -77,6 +85,14 @@ def encode(content, encoding: Optional[str] = "json") -> Response:
         return Response(
             (chunk for chunk in (orjson_encode(content),)),  # generator
             headers={"Content-Type": "application/json"},
+        )
+    elif encoding == "bson":
+        return Response(
+            (chunk for chunk in (bson_encode(content),)),  # generator
+            headers={
+                "Content-Type": "application/bson",
+                "Content-Disposition": 'attachment; filename="data.bson"',
+            },
         )
     elif encoding == "npy":
         return Response(
