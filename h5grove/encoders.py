217299a0c5d0c4185845808a13cdbea5c7d31dec
cherbon1 <37700955+cherbon1@users.noreply.github.com>
2022-04-22 10:26:33 +0200

    Fix handling of np.float128
    Some distributions of numpy don't work with float128 (see https://stackoverflow.com/questions/29820829/cannot-use-128bit-float-in-python-on-64bit-architecture)

--- h5grove/encoders.py
+++ h5grove/encoders.py
@@ -23,8 +23,8 @@ def orjson_default(o: Any) -> Union[list, float, str, None]:
     """Converts Python objects to JSON-serializable objects.
 
     :raises TypeError: if the object is not supported."""
-    if isinstance(o, np.float128):
-        # float128 is not converted to native float by NumPy so we need to force it even if it means loosing precision
+    if hasattr(np, 'float128') and isinstance(o, np.float128):
+        # float128 is not converted to native float by NumPy so we need to force it even if it means losing precision
         return float(o)
     if isinstance(o, (np.generic, np.ndarray)):
         return o.tolist()
