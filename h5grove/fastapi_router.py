870d296628b3b0a253ca7a7711ad416aad48f62e
Loic Huder <loic.huder@esrf.fr>
2022-06-07 11:33:00 +0200

    Fix file closing issues during tests

--- h5grove/fastapi_router.py
+++ h5grove/fastapi_router.py
@@ -15,43 +15,41 @@ async def add_base_path(file):
     return f"{base_path}/{file}" if base_path else file
 
 
-async def get_h5file(file: str = Depends(add_base_path)):
-    return h5py.File(file, mode="r")
-
-
 @router.get("/attr/")
 async def get_attr(
-    file: h5py.File = Depends(get_h5file),
+    file: str = Depends(add_base_path),
     path: str = "/",
     attr_keys: Optional[List[str]] = Query(default=None),
 ):
-    content = create_content(file, path)
-    assert isinstance(content, ResolvedEntityContent)
-    return content.attributes(attr_keys)
+    with h5py.File(file, "r") as h5file:
+        content = create_content(h5file, path)
+        assert isinstance(content, ResolvedEntityContent)
+        return content.attributes(attr_keys)
 
 
 @router.get("/data/")
 async def get_data(
-    file: h5py.File = Depends(get_h5file),
+    file: str = Depends(add_base_path),
     path: str = "/",
     dtype: str = "origin",
     format: str = "json",
     flatten: bool = False,
     selection=None,
 ):
-    content = create_content(file, path)
-    assert isinstance(content, DatasetContent)
-    data = content.data(selection, flatten, dtype)
-    return (
-        data
-        if format != "bin"
-        else Response(data.tobytes(), media_type="application/octet-stream")
-    )
+    with h5py.File(file, "r") as h5file:
+        content = create_content(h5file, path)
+        assert isinstance(content, DatasetContent)
+        data = content.data(selection, flatten, dtype)
+        return (
+            data
+            if format != "bin"
+            else Response(data.tobytes(), media_type="application/octet-stream")
+        )
 
 
 @router.get("/meta/")
 async def get_meta(
-    file: h5py.File = Depends(get_h5file),
+    file: str = Depends(add_base_path),
     path: str = "/",
     resolve_links: str = "only_valid",
 ):
@@ -59,14 +57,16 @@ async def get_meta(
         resolve_links,
         fallback=LinkResolution.ONLY_VALID,
     )
-    con = create_content(file, path)
-    return con.metadata()
+    with h5py.File(file, "r") as h5file:
+        con = create_content(h5file, path)
+        return con.metadata()
 
 
 @router.get("/stats/")
 async def get_stats(
-    file: h5py.File = Depends(get_h5file), path: str = "/", selection=None
+    file: str = Depends(add_base_path), path: str = "/", selection=None
 ):
-    content = create_content(file, path)
-    assert isinstance(content, DatasetContent)
-    return content.data_stats(selection)
+    with h5py.File(file, "r") as h5file:
+        content = create_content(h5file, path)
+        assert isinstance(content, DatasetContent)
+        return content.data_stats(selection)
